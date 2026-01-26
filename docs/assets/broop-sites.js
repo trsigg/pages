b7f0bdb69220bbc79abeb687c8f8ca1a7bd233be
Tynan Sigg <trsigg@gmail.com>
2026-01-26 05:56:12 +0000

    broop: block catgpt, recursive squouch

--- docs/assets/broop-sites.js
+++ docs/assets/broop-sites.js
@@ -161,7 +161,7 @@ window.BROOP_SITES = {
       children: [
         { weight: 1, url: "https://www.nytimes.com/2025/11/04/us/politics/dick-cheney-dead.html" },
         { weight: 1, url: "https://buybears.org/" },
-        { weight: 1, url: "https://catgpt.wvd.io/" },
+        { weight: 1, url: "https://catgpt.wvd.io/", blocked: true },
         { weight: 0.2, url: "https://chatgpt.com/share/6975e5f9-823c-8006-8946-462d96363106", blocked: true }, // elon child
         { weight: 1, url: "https://grok.com/share/c2hhcmQtNA_fc92734d-89f3-4019-ae7f-83efd7308dc0", blocked: true }, // altman child
         { weight: 0.2, url: "https://docs.google.com/spreadsheets/d/1A2AAzbg2b2dDJuH3YkFBsYzQy3eNTvXAimUEbTc2cWc/edit?gid=1028301268#gid=1028301268" }, // movie votes
