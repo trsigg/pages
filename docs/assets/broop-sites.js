aabfd8578980178716fb460d39d2f6a47526405e
Tynan Sigg <trsigg@gmail.com>
2026-01-26 06:41:59 +0000

    broop: reweighting/reorganization

--- docs/assets/broop-sites.js
+++ docs/assets/broop-sites.js
@@ -161,12 +161,24 @@ window.BROOP_SITES = {
       children: [
         { weight: 1, url: "https://www.nytimes.com/2025/11/04/us/politics/dick-cheney-dead.html" },
         { weight: 1, url: "https://buybears.org/" },
-        { weight: 1, url: "https://catgpt.wvd.io/", blocked: true },
-        { weight: 0.2, url: "https://chatgpt.com/share/6975e5f9-823c-8006-8946-462d96363106", blocked: true }, // elon child
-        { weight: 1, url: "https://grok.com/share/c2hhcmQtNA_fc92734d-89f3-4019-ae7f-83efd7308dc0", blocked: true }, // altman child
-        { weight: 0.2, url: "https://docs.google.com/spreadsheets/d/1A2AAzbg2b2dDJuH3YkFBsYzQy3eNTvXAimUEbTc2cWc/edit?gid=1028301268#gid=1028301268" }, // movie votes
-        { weight: 1, url: "https://partiful.com/e/XG5H1zQy2PGelmLwtYr5", blocked: true }, // sock wrestling event
-        { weight: 1, url: "https://en.wikipedia.org/wiki/Sock_wrestling" }, // sock wrestling reddit
+        // chats
+        {
+          weight: 1,
+          children: [
+            { weight: 1, url: "https://catgpt.wvd.io/", blocked: true },
+            { weight: 1, url: "https://chatgpt.com/share/6975e5f9-823c-8006-8946-462d96363106", blocked: true }, // elon child
+            { weight: 1, url: "https://grok.com/share/c2hhcmQtNA_fc92734d-89f3-4019-ae7f-83efd7308dc0", blocked: true }, // altman child
+          ],
+        },
+        { weight: 1, url: "https://docs.google.com/spreadsheets/d/1A2AAzbg2b2dDJuH3YkFBsYzQy3eNTvXAimUEbTc2cWc/edit?gid=1028301268#gid=1028301268" }, // movie votes
+        // sock wrestling
+        {
+          weight: 1,
+          children: [
+            { weight: 1, url: "https://partiful.com/e/XG5H1zQy2PGelmLwtYr5", blocked: true }, // sock wrestling event
+            { weight: 1, url: "https://en.wikipedia.org/wiki/Sock_wrestling" }, // sock wrestling reddit
+          ],
+        },
         { weight: 1, url: "https://trsigg.github.io/pages/broop/" }, // oh god recursion
       ],
     },
